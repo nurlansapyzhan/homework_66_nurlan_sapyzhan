5182b75916f4a6b85fac6f9563be67bd832c51fb
Nurlan Sapyzhan <nurlan_sapyzhan@mail.ru>
2023-04-01 20:42:47 +0600

    Bonus

--- task3.js
+++ task3.js
@@ -1,6 +1,7 @@
 container = document.getElementById('container')
+let colors = ['red', 'blue', 'yellow', 'green', 'cyan']
 for (let i = 1; i < 6; i++) {
     const elementDiv = document.createElement('div')
-    elementDiv.innerHTML = `<div class="element">Element ${i}</div>`
+    elementDiv.innerHTML = `<div class="element">Element <span style="color: ${colors[i-1]}">${i}</span></div>`
     container.appendChild(elementDiv)
 }
