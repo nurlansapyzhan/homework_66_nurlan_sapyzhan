06edf67e2b9e74a9c5192d9d13b45dfb13848cd7
Nurlan Sapyzhan <nurlan_sapyzhan@mail.ru>
2023-04-01 19:56:52 +0600

    Task 1

---
+++ task1.js
@@ -0,0 +1,6 @@
+console.log(document.getElementById('string-3').innerText)
+console.log(document.getElementById('string-5').innerText)
+console.log(document.getElementById('string-2').innerText)
+console.log(document.getElementById('string-6').innerText)
+console.log(document.getElementById('string-4').innerText)
+console.log(document.getElementById('string-1').innerText)
